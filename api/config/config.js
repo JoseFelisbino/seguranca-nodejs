946474f65fd7530916c850025ceb5031372d4bf2
JoseFelisbino <lu31660@gmail.com>
2026-09-13 15:10:04 -0300

    fix: muda para aquivos .js

---
+++ api/config/config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../../.env')
+});
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    dialect: 'postgres'
+  },
+
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'seguranca_nodejs_test',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    dialect: 'postgres'
+  },
+
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'seguranca_nodejs_production',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    dialect: 'postgres'
+  }
+};
