20dd86d9770f79cd1416d96c2c56c957a1f736a8
JoseFelisbino <lu31660@gmail.com>
2026-09-13 19:26:42 -0300

    feat: adiciona gereção de JWT

--- api/services/authService.js
+++ api/services/authService.js
@@ -1,5 +1,7 @@
 const database = require('../models')
-
+const { compare } = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
+const jsonSecret = require('../config/jsonSecret')
 class AuthService {
     async login(dto) {
         const usuario = await database.usuarios.findOne({
@@ -11,8 +13,22 @@ class AuthService {
 
         if (!usuario) {
             throw new Error("Usuário não cadastrado");
-            
         }
+
+        const senhaIguais = await compare(dto.senha, usuario.senha);
+
+        if (!senhaIguais) {
+            throw new Error("Usuario ou senha invalido");
+        }
+
+        const accessToken = sign({
+            id: usuario.id,
+            email: usuario.email
+        }, jsonSecret.secret, {
+            expiresIn: 86400
+        })
+
+        return {accessToken};
     }
 }
 
