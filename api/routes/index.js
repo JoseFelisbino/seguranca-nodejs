3446759d798f34444880e62a6712e3271827056b
JoseFelisbino <lu31660@gmail.com>
2026-09-13 18:59:44 -0300

    feat: adiciona rota auth

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,14 @@ const bodyParser = require('body-parser')
  
 const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoutes');
+const auth = require('./authRoutes');
 
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     produto,
-    usuario
+    usuario,
+    auth
   )
 }
