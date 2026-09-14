c4fb610a68f6cb44ee7dc9ee0290291344a0eb23
JoseFelisbino <lu31660@gmail.com>
2026-09-13 22:09:53 -0300

    feat: adiciona rotas de roles

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser')
 const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoutes');
 const auth = require('./authRoutes');
+const role = require('./roles');
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     auth,
+    usuario,
     produto,
-    usuario
-  )
+    role
+  );
 }
