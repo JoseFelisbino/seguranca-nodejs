301632ce1a21bea59db0fe29e240c4214b8fc632
JoseFelisbino <lu31660@gmail.com>
2026-09-13 22:55:40 -0300

    feat: adiciona rota de deletar role

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -7,7 +7,7 @@ router
     .post('/roles', RoleController.cadastrar)
     .get('/role', RoleController.buscarTodosRoles)
     .get('/role/:id', RoleController.buscarRolePorId)
-    .delete('/role/:id')
+    .delete('/role/:id', RoleController.deletarRolePorId)
     .put('/role/:id')
 
 module.exports = router;
