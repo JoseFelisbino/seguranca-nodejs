15a6fe63a3ebda00a97616fbb9d6b18b92199464
JoseFelisbino <lu31660@gmail.com>
2026-09-19 21:47:28 -0300

    feat: adiciona controller de cadastrar permissao

---
+++ api/controllers/permissaoController.js
@@ -0,0 +1,20 @@
+const PermissaoService = require('../services/permissaoService');
+
+const permissaoService = new PermissaoService();
+
+class PermissaoController {
+    static async cadastrar(req, res) {
+        const { nome, descricao} = req.body;
+
+        try {
+            const permissao = await permissaoService.cadastrarPermissao({nome, descricao});
+
+            res.status(201).send(permissao);
+        } catch (error) {
+            res.status(400).send({message: error.message});
+        }
+    }
+
+}
+
+module.exports = PermissaoController
