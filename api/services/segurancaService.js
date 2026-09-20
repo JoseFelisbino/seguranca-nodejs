17a136df3e5377dd4a0ac59d9ae3153ba5666de8
JoseFelisbino <lu31660@gmail.com>
2026-09-20 00:32:54 -0300

    feat: inicio de cadastro, perfil e permissoes

---
+++ api/services/segurancaService.js
@@ -0,0 +1,26 @@
+const database = require('../models')
+
+class SegurancaService {
+    async cadastrarAcl(dto) {
+        const usuario = await database.usuarios.findOne({
+            include: [
+                {
+                    model: database.roles,
+                    as: 'usuario_roles',
+                    attributes: ['id', 'nome', 'descricao']
+                },
+                {
+                    model: database.permissoes,
+                    as: 'usuario_permissoes',
+                    attributes: ['id', 'nome', 'descricao']
+                }
+            ],
+            where: {
+                id: dto.usuario.Id
+            }
+        })
+
+    }
+}
+
+module.exports = SegurancaService;
